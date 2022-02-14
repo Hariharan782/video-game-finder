c765a7d9f5120fbca2d6b7b5d4787fe5fcf66bc9
hariharan K <iamhari1997@gmail.com>
2022-02-15 00:18:13 +0530

    http model and service created

---
+++ src/app/model.ts
@@ -0,0 +1,39 @@
+export interface Game {
+  background_image: string;
+  name: string;
+  released: string;
+  metacritic_url: string;
+  website: string;
+  description: string;
+  metacritic: number;
+  genres: Array<Genre>;
+  parent_platform: Array<ParentPlatform>;
+  publishers: Array<Publishers>;
+  ratings: Array<Ratings>;
+  screenshot: Array<Screenshot>;
+  trailers: Array<Trailer>;
+}
+
+export interface APIResponse<T> {
+  results: Array<T>;
+}
+interface Genre {
+  name: string;
+}
+interface ParentPlatform {
+  platform: { name: string };
+}
+interface Publishers {
+  name: string;
+}
+interface Ratings {
+  id: number;
+  count: number;
+  title: string;
+}
+interface Screenshot {
+  image: string;
+}
+interface Trailer {
+  data: { max: string };
+}
